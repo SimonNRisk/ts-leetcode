d9f66ce524c455e8610c479314603faa78957d48
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-07-12 16:53:49 -0400

    iterative dft

--- trees.js
+++ trees.js
@@ -19,3 +19,26 @@ a.right = c;
 b.left = d;
 b.right = e;
 c.right = f;
+
+//DFT: a, b, d, e, c, f
+//stack: FIFO (add to top, remove from top)
+//push right child first so left child (which we visit first) is top of stack
+
+export const depthFirstTraversal = (root) => {
+  const stack = [root]; //just an array as long as we use push and pop
+  const result = [];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    result.push(current.val);
+
+    if (current.right) {
+      stack.push(current.right);
+    }
+    if (current.left) {
+      stack.push(current.left);
+    }
+  }
+  return result;
+};
+
+console.log(depthFirstTraversal(a));
