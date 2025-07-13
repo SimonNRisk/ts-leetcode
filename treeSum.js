00d9db1ee453ebbce8a87e637fd79b1c3375eae4
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-07-12 22:28:51 -0400

    tree sum iteratively

---
+++ treeSum.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+
+b.left = d;
+b.right = e;
+c.right = f;
+
+export const treeSum = (root) => {
+  let sum = 0;
+  const stack = [root];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    sum += current.val;
+    if (current.right) {
+      stack.push(current.right);
+    }
+    if (current.left) {
+      stack.push(current.left);
+    }
+  }
+  return sum;
+};
+
+console.log(treeSum(a));
